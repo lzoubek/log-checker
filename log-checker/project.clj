ae48e7f78cadcdb78b8118dde90516b6c6a59e30
Libor Zoubek <lzoubek@redhat.com>
2012-11-05 09:59:35 +0100

    fixed src-path so jar uploaded to clojars actually contains classes!

--- log-checker/project.clj
+++ log-checker/project.clj
@@ -1,6 +1,6 @@
 (defproject com.redhat.qe/log-checker "1.0.0-SNAPSHOT"
   :description "TestNG Listener with annotations for checking local and remote log files"
-  :java-source-path "src/main/java"
+  :java-source-paths ["src/main/java"]
   :javac-options {:debug "on"}
   :dependencies [[org.testng/testng "6.5.1"] 
                  [com.redhat.qe/ssh-tools "1.0.0"]])
