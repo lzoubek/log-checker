5c7d4e4de0b6e66fd4d8432c7bc7a569b3dbeb40
Libor Zoubek <lzoubek@redhat.com>
2013-03-22 10:39:32 +0100

    version++

--- log-checker/project.clj
+++ log-checker/project.clj
@@ -1,6 +1,6 @@
-(defproject com.redhat.qe/log-checker "1.0.0-SNAPSHOT"
+(defproject com.redhat.qe/log-checker "1.0.1-SNAPSHOT"
   :description "TestNG Listener with annotations for checking local and remote log files"
   :java-source-paths ["src/main/java"]
-  :javac-options {:debug "on"}
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :dependencies [[org.testng/testng "6.5.1"] 
                  [com.redhat.qe/ssh-tools "1.0.0"]])
